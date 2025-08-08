cb217a2c5b019ea50ec72e15456813cc8121b61c
PIR AHMED MEHMOOD <pirahmedmehmood786@gmail.com>
2025-08-08 11:56:53 +0500

    Ch 01 Assignment JS complete

---
+++ Javascript Assignment 1/app.js
@@ -0,0 +1,30 @@
+//CHAPTER:NO:01
+// Question:No:1//
+setTimeout(() => {
+    alert("Assalamo Alaikum Sir! Welcome to My Website");
+}, 500);
+//Question:No:2
+setTimeout(() => {
+    alert("Error! Please enter a valid password");
+}, 1500);
+//Question:No:3
+setTimeout(() => {
+    alert("Welcome to JS Land...\nHappy Coding!");
+}, 2500);
+//Question:No:4
+setTimeout(() => {
+    alert("Welcome to JS Land");
+}, 3500);
+setTimeout(() => {
+    alert("Happy Coding!");
+}, 4500);
+
+//Question:No:5
+setTimeout(() => {
+    alert("Hello... I can run JS through my web browser's console");
+}, 5500);
+//Question:No:6
+setTimeout(() => {
+    alert("Welcome to\nmy website");
+}, 6500);
+//Question:No:7 has be completed and ch:no: 1
